e2d4e50abc4ee6af8a61f9e8d760da76d381cb08
Jose Guerra Arteta <joselguerra@Joses-MacBook-Air.local>
2017-08-03 12:50:04 -0700

    Creating function skeleton

--- smallLargeCity.js
+++ smallLargeCity.js
@@ -1,2 +1,7 @@
 // Small or Large city - function
 const whatCities = { vancouver: 603502, burnaby: 223220, langley: 104177, surrey: 705000, abbotsford: 100000 };
+
+function sizeOfCity(cities) {
+  
+  return;
+}
